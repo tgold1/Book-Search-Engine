a6b0e7d920ea50b130d30cf6a6e84e7b655db2f8
Tim Gold <timothygold@comcast.net>
2023-05-24 19:34:58 -0400

    added a const password in resolvers.js in mutation to check for password

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -27,17 +27,23 @@ const resolvers = {
     },
 
     Mutation: {
-        addUser: async (parent, {username, email }) => {
-            const user = await User.create({ username, email});
+        addUser: async (parent, {username, email, password }) => {
+            const user = await User.create({ username, email, password});
             const token = signToken(user);
             return { token, user };
         },
-        login: async (parent, { email }) => {
+        login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
 
             if (!user) {
                 throw new AuthenticationError('No user found with this email address');
             }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+              }
 
             const token = signToken(user)
 
