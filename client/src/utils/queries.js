5adc26346b7e9dbe06f636efc5c26a77bcc4d8bf
Tim Gold <timothygold@comcast.net>
2023-05-25 09:55:53 -0400

    added a mutations.js file and added mutations for user and book, author adjustment in queries and npm install

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -8,7 +8,7 @@ export const QUERY_ME = gql`
             email
             books {
                 bookId
-                [authors]
+                authors
                 description 
                 title 
                 image
